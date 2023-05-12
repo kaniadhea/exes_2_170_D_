4f7513a8fbcf2237f77c30aad74f31e6438cb027
kaniadhea <kaniadhea7@gmail.com>
2023-05-11 20:36:37 -0700

    Add project files.

---
+++ exes_2_170_D_/exes_2_170_D_.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int dhea[30];		//Membuat Array dengan panjang data 30
+int n;				//Membuat Varibel input n 
+
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan jumlah data pada Array : ";      
+        cin >> "memanggil variable inputan n : ";
+    }
+
+    int main() {
+        if  mid = (lowerbound + upperbound) // 2
+
+            if arr[mid] == desired_element :
+        return "Found"
+
+            if desired_element < arr[mid] :
+                upperbound = mid - 1
+            else :
+                lowerbound = mid + 1;
+    };
+      
+};
+
+
+
+
